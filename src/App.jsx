b84c409028d22729d6acdf5679327d6f34fc9804
carled7 <carlos_ed03@outlook.com>
2022-11-06 06:01:34 -0300

    telas finalizadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import {BrowserRouter as Router,Routes,Route } from 'react-router-dom'
-import {Home} from './pages/home/home'
-import {Tickets} from './pages/tickets/tickets'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/home/home'
+import { Tickets } from './pages/tickets/tickets'
+import { Rate } from './pages/rate/rate';
 
 export function App() {
 
   return (
-    
+
     <Router>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/tickets' element={<Tickets />} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/tickets' element={<Tickets />} />
+        <Route path='/rate' element={<Rate/>}/>
 
+      </Routes>
+    </Router>
 
-    </Routes>
-  </Router>
-  
   )
 }
 
